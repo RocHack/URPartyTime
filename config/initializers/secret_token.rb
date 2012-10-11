3c6389b6ded943d2ba03f81a6a96fbde859f9daa
Francis Hinson <francis@orchive.com>
2012-10-11 11:42:00 -0400

    setting things up, ill be working on this later this weekend

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Urpartytime::Application.config.secret_token = '4706953e908474342ddd19e03eb2ea02b5d85bdee344320fa84e468dbad96cf638e40a9e6e20cce2b457b946d989016617bbe3d2547cf5ed03436b2e1fcd5ae5'
